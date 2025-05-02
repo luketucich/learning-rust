695bc06c377b9b164917d1dab58eca9ffdfd58a0
luketucich <luketucich@gmail.com>
2025-05-02 14:39:16 -0400

    chore: Completed 3.1 lesson on variables.

---
+++ chapter_3/variables/target/debug/deps/variables-78b6df3f2866972a.d
@@ -0,0 +1,5 @@
+/Users/luke/repos/learning-rust/chapter_3/variables/target/debug/deps/variables-78b6df3f2866972a: src/main.rs
+
+/Users/luke/repos/learning-rust/chapter_3/variables/target/debug/deps/variables-78b6df3f2866972a.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/luke/repos/learning-rust/chapter_3/variables/target/debug/variables: /Users/luke/repos/learning-rust/chapter_3/variables/src/main.rs
